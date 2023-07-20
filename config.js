2ba9c38448695e27b8c391a99a8c7fca39bd83c8
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2023-07-20 16:34:49 +0530

    Removed the code to check the connection established and moved into other file

--- config.js
+++ config.js
@@ -1,16 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize =   new Sequelize('meghana_db', 'meghanagaddam', '', {
+const db = require('./models/employee');
+const sequelize = new Sequelize('meghana_db', 'meghanagaddam', '', {
     host: 'localhost',
     dialect: 'postgres'
 });
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log(
-      "Connection to the database has been established successfully."
-    );
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
+module.exports = sequelize;
