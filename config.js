3f160b4ee40420b07cd7b6f0159ad7e95bee211a
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2023-07-20 16:35:59 +0530

    Removed the import statements that are unused

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const db = require('./models/employee');
 const sequelize = new Sequelize('meghana_db', 'meghanagaddam', '', {
     host: 'localhost',
     dialect: 'postgres'
