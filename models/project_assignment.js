7c0c756ca37635b560669ce54909c20f2e40820b
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2023-07-22 17:33:08 +0530

    Added foreign keys in the project_assignment model

--- models/project_assignment.js
+++ models/project_assignment.js
@@ -1,5 +1,9 @@
 const sequelize = require("../config");
 const { DataTypes, Model } = require("sequelize");
+ const Employee = require("./employee");
+ const Project = require("./project");
+ const Role = require("./role");
+
 
 class ProjectAssignment extends Model {}
 
@@ -12,6 +16,28 @@ ProjectAssignment.init(
     },
     contribution_percentage: {
       type: DataTypes.INTEGER,
+    },
+
+    EmployeeId: {
+      type: DataTypes.STRING,
+      references: {
+        model: Employee,
+        key: "employee_id",
+      }
+    },
+    ProjectId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Project,
+        key: "project_id", 
+      },
+    },
+    RoleId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Role,
+        key: "role_id", 
+      }
     },
   },
   {
