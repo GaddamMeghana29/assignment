0e0eb23d01be0f96b44d5730268dcb8f8e9d7b67
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2023-07-23 22:08:22 +0530

    Added script to import data from CSV files

---
+++ insertData.js
@@ -0,0 +1,29 @@
+const csv = require("csvtojson");
+const Employee = require("./models/employee"); 
+const Project = require("./models/project"); 
+const ProjectAssignment = require("./models/project_assignment"); 
+
+const employeesCSVFilePath =
+  "csv_data/employees.csv";
+const projectsCSVFilePath = "csv_data/projects.csv";
+const projectAssignmentsCSVFilePath = "csv_data/project_assignment.csv";
+
+const importData = async () => {
+  try {
+    const employees = await csv().fromFile(employeesCSVFilePath);
+    const projects = await csv().fromFile(projectsCSVFilePath);
+    const projectAssignments = await csv().fromFile(
+      projectAssignmentsCSVFilePath
+    );
+
+    await Employee.bulkCreate(employees, { ignoreDuplicates: true });
+    await Project.bulkCreate(projects, { ignoreDuplicates: true });
+    await ProjectAssignment.bulkCreate(projectAssignments, { ignoreDuplicates: true });
+
+    console.log("Data import completed successfully.");
+  } catch (error) {
+    console.error("Error importing data:", error);
+  }
+};
+
+importData();
