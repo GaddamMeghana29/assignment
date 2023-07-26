d3455c93c0238713dee9b878ca5f7a2baf0d2d5b
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2023-07-26 21:37:41 +0530

    Added function calling statements and also imported them

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const sequelize = require("./config.js");
+const { insertDataFromCSV } = require("./insertData.js");
+const { getEmployeesNotPartOfAnyProject} = require("./reports.js")
 
-  const nconf = require("nconf");
-  const path = require("path");
-  const Employee = require("./models/employee");
-  const Project = require("./models/project");
-  const ProjectAssignment = require("./models/project_assignment");
-  nconf.file({ file: path.join(__dirname, "config.json") });
- 
+const nconf = require("nconf");
+const path = require("path");
 
- const DB_NAME = nconf.get("DB_NAME");
- const DB_USERNAME = nconf.get("DB_USERNAME");
- const DB_PASSWORD = nconf.get("DB_PASSWORD");
- const DB_HOST = nconf.get("DB_HOST");
- const DB_DIALECT = nconf.get("DB_DIALECT");
- const DB_logging = nconf.get("logging");
+nconf.file({ file: path.join(__dirname, "config.json") });
 
+const DB_NAME = nconf.get("DB_NAME");
+const DB_USERNAME = nconf.get("DB_USERNAME");
+const DB_PASSWORD = nconf.get("DB_PASSWORD");
+const DB_HOST = nconf.get("DB_HOST");
+const DB_DIALECT = nconf.get("DB_DIALECT");
+const DB_logging = nconf.get("logging");
 sequelize
   .authenticate()
   .then(async () => {
     console.log(
       "Connection to the database has been established successfully."
     );
-    await sequelize.sync({force:true});
     console.log("Models have been synchronized with the database.");
+    insertDataFromCSV();
+    getEmployeesNotPartOfAnyProject(); 
+    
   })
   .catch((error) => {
     console.error("Unable to connect to the database:", error);
